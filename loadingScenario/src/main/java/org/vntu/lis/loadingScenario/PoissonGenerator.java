575eec1e495e53a207320bbe26ad0966cc4ac546
Oleksandr Khoshaba <Oleksandr.Khoshaba@gmail.com>
2023-03-26 19:47:29 +0300

    Change interface (edit getter) in ICoordX interface.

--- loadingScenario/src/main/java/org/vntu/lis/loadingScenario/PoissonGenerator.java
+++ loadingScenario/src/main/java/org/vntu/lis/loadingScenario/PoissonGenerator.java
@@ -3,9 +3,9 @@ package org.vntu.lis.loadingScenario;
 import java.util.ArrayList;
 
 public class PoissonGenerator implements ICoordX {
+    ArrayList arrayList = new ArrayList();
 
     public ArrayList<Long> getArrayListX() {
-        ArrayList arrayList = new ArrayList();
         return arrayList;
     }
 }
