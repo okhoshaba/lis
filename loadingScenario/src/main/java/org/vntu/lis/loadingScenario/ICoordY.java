618e85121a94b7e9790835397163622a41ecaa4a
Oleksandr Khoshaba <Oleksandr.Khoshaba@gmail.com>
2023-03-29 14:41:39 +0300

    Init ICoordY interface

---
+++ loadingScenario/src/main/java/org/vntu/lis/loadingScenario/ICoordY.java
@@ -0,0 +1,8 @@
+package org.vntu.lis.loadingScenario;
+
+import java.util.ArrayList;
+
+public interface ICoordY {
+    ArrayList<Long> getYArrayList();
+    void setYArrayList(int firstOption, int secondOption, int thirdOption);
+}
