ee7f167acae9621ece883c4bab7783bb779f3813
Oleksandr Khoshaba <Oleksandr.Khoshaba@gmail.com>
2023-03-29 16:54:18 +0300

    Add to TestTrajectory class CoordY object

--- loadingScenario/src/main/java/org/vntu/lis/loadingScenario/TestTrajectory.java
+++ loadingScenario/src/main/java/org/vntu/lis/loadingScenario/TestTrajectory.java
@@ -9,23 +9,24 @@ public class TestTrajectory {
         );
 
         CoordX coordX = context.getBean("coordX", CoordX.class);
+        CoordY coordY = context.getBean("coordY", CoordY.class);
 
         // For diagnose purposes only  ??? It needs to be in a separate group
         // (For example: Diagnose input parameters).
 //        System.out.println(coordX.getStep());
+        System.out.println("coordY, x parameter: " + coordY.getxParametr());
+        System.out.println("coordY, b parameter: " + coordY.getbParametr());
 
         // Business Logics for ClassicUniformGenerator
         // For diagnose only
         ICoordX classicUniformGenerator = new ClassicUniformGenerator();
         classicUniformGenerator.setXArrayList(coordX.getNumber(), coordX.getPeriod(), coordX.getSeries());
-//        System.out.println(classicUniformGenerator.toString());
         System.out.println("Coordinate X (in ClassicGenerator): " + classicUniformGenerator.getXArrayList());
 
         // Business Logics for DefineUniformGenerator
         // For diagnose only
         ICoordX defineUniformGenerator = new DefineUniformGenerator();
         defineUniformGenerator.setXArrayList(coordX.getStep(), coordX.getPeriod(), coordX.getSeries());
-//        System.out.println(defineUniformGenerator.toString());
         System.out.println("Coordinate X (in UniformGenerator): " + defineUniformGenerator.getXArrayList());
 
         context.close();
