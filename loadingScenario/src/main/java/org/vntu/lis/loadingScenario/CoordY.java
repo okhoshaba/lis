cfe05d51ce69c3a409a68ec913d3c1eec440cc1c
Oleksandr Khoshaba <Oleksandr.Khoshaba@gmail.com>
2023-04-05 21:11:27 +0300

    Change c2 and c3 parameters in ClassicUniformGenerator

--- loadingScenario/src/main/java/org/vntu/lis/loadingScenario/CoordY.java
+++ loadingScenario/src/main/java/org/vntu/lis/loadingScenario/CoordY.java
@@ -3,10 +3,11 @@ package org.vntu.lis.loadingScenario;
 import java.util.ArrayList;
 
 public class CoordY {
-    private ArrayList<Long> arrayList = new ArrayList<Long>();
+//    private ArrayList<Long> arrayList = new ArrayList<Long>();
 
     private ICoordY iCoordY;
     private double c1Parametr, c0Parametr;
+    private double c2Parametr, c3Parametr;
 
     public ICoordY getiCoordY() {
         return iCoordY;
@@ -29,6 +30,22 @@ public class CoordY {
 
     public void setC0Parametr(double c0Parametr) {
         this.c0Parametr = c0Parametr;
+    }
+
+    public double getC2Parametr() {
+        return c2Parametr;
+    }
+
+    public void setC2Parametr(double c2Parametr) {
+        this.c2Parametr = c2Parametr;
+    }
+
+    public double getC3Parametr() {
+        return c3Parametr;
+    }
+
+    public void setC3Parametr(double c3Parametr) {
+        this.c3Parametr = c3Parametr;
     }
 
     // !!! Change for another Class !!!
